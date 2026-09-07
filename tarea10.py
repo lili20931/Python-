faede00998311b648bebe9a376b555fcb5c99299
Lucianovd8 <lucianofrutos2008@gmail.com>
2026-09-07 10:08:03 -0300

    07/09/26

--- tarea10.py
+++ tarea10.py
@@ -1,18 +1,18 @@
 print("Se sacara un promedio de los números positivos.\n Ingrese números mayor a cero.\n Si presiona 0 se corta el programa.\n")
 acum = 0
 suma = 0
-var = True
+corte = True
 promedio = 0
-while var != 0:
+while corte != 0:
     valor1 = float(input("Ingrese un número: "))
     if valor1 > 0:
         acum = acum + 1
         suma = suma + valor1
-        promedio = (suma / acum)
         print(suma)
     elif valor1 < 0: 
         print("No cumple con lo especificado")
     else:
-        var = False 
+        corte = False 
+promedio = (suma / acum)
 print(promedio)
 print("FIN")
